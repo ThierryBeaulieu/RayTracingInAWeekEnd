3aa9a60433b855f39c0e00fadb255fc89bf5d560
ThierryBeaulieu <thierry.abeaulieu@gmail.com>
2025-08-27 23:05:44 -0700

    add logger

--- PathTracer/Program.cs
+++ PathTracer/Program.cs
@@ -1,11 +1,21 @@
-﻿
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using PathTracer;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Renderer renderer = new();
+        var serviceProvider = new ServiceCollection()
+            .AddLogging(config =>
+            {
+                config.AddConsole();
+                config.SetMinimumLevel(LogLevel.Debug);
+            })
+            .AddTransient<Renderer>()
+            .BuildServiceProvider();
+
+        var renderer = serviceProvider.GetRequiredService<Renderer>();
         renderer.Render();
     }
 }
--- PathTracer/Renderer.cs
+++ PathTracer/Renderer.cs
@@ -1,21 +1,30 @@
 using System;
+using System.IO;
+using Microsoft.Extensions.Logging;
 
 namespace PathTracer;
 
-public class Renderer
+public class Renderer(ILogger<Renderer> logger)
 {
+    private readonly ILogger<Renderer> _logger = logger;
+    private readonly string filePath = "result.ppm";
+
 
     public readonly int image_width = 256;
     public readonly int image_height = 256;
 
     public void Render()
     {
-        Console.WriteLine("P3");
-        Console.WriteLine(image_width + " " + image_height);
-        Console.WriteLine("255");
+
+        using StreamWriter writer = new(filePath);
+
+        writer.WriteLine("P3");
+        writer.WriteLine(image_width + " " + image_height);
+        writer.WriteLine("255");
 
         for (int row = 0; row < image_height; row++)
         {
+            _logger.Log(LogLevel.Debug, "ScanLines remaining " + (image_height - row));
             for (int col = 0; col < image_width; col++)
             {
                 double r = (double)row / (image_width - 1);
@@ -26,8 +35,10 @@ public class Renderer
                 int ig = (int)(255.999 * g);
                 int ib = (int)(255.999 * b);
 
-                Console.WriteLine(ir + " " + ig + " " + ib);
+                writer.WriteLine(ir + " " + ig + " " + ib);
             }
         }
+        _logger.Log(LogLevel.Debug, "Done");
+
     }
 }
